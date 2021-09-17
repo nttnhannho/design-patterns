da30eb2cee82ad52890cd5497c7917206ec90856
nttnhannho <makelifeperfect@gmail.com>
2021-09-17 18:55:19 +0700

    Simplify the implementation of this pattern

--- factory_method_design_pattern.py
+++ factory_method_design_pattern.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from random import randrange
 
 
 class Species(ABC):
@@ -43,52 +42,29 @@ class Durian(Species):
         return "Durian"
 
 
-class SpeciesFactory(ABC):
-    @abstractmethod
-    def select_species(self):
-        pass
+class NoneSpecies(Species):
+    def show_name(self):
+        return "Invalid species"
 
 
-class HighlandsFactory(SpeciesFactory):
-    def select_species(self):
-        num = randrange(3)
-        if num == 0:
-            return Coffee()
-        if num == 1:
-            return Pepper()
-        if num == 2:
-            return Rubber()
-
-
-class MekongFactory(SpeciesFactory):
-    count = 0
-
-    def select_species(self):
-        if MekongFactory.count == 0:
-            MekongFactory.count += 1
-            return Orange()
-        if MekongFactory.count == 1:
-            MekongFactory.count += 1
-            return Mango()
-        if MekongFactory.count == 2:
-            MekongFactory.count += 1
-            return Longan()
-        if MekongFactory.count == 3:
-            MekongFactory.count = 0
-            return Durian()
+class SpeciesFactory:
+    @staticmethod
+    def get_species(type_):
+        switcher = {
+            "Coffee": Coffee(),
+            "Pepper": Pepper(),
+            "Rubber": Rubber(),
+            "Orange": Orange(),
+            "Longan": Longan(),
+            "Mango": Mango(),
+            "Durian": Durian()
+        }
+        return switcher.get(type_, NoneSpecies())
 
 
 if __name__ == "__main__":
-    print("Highlands:")
-    hls_factory = HighlandsFactory()
-    for i in range(1, 13):
-        species = hls_factory.select_species()
-        print(f"The farmer number {i} received {species.show_name()}")
-
-    print("*" * 50)
-
-    print("Mekong:")
-    mekong_factory = MekongFactory()
-    for i in range(1, 13):
-        species = mekong_factory.select_species()
-        print(f"The farmer number {i} received {species.show_name()}")
+    factory = SpeciesFactory()
+    species = factory.get_species("Coffee")
+    print(species.show_name())
+    species = factory.get_species("Tree")
+    print(species.show_name())
