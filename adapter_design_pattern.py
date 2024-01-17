d9b23668a921dc37c3af2a8d9e77b194f764d438
nttnhannho <makelifeperfect@gmail.com>
2024-01-17 23:54:36 +0700

    Update adapter design pattern

--- adapter_design_pattern.py
+++ adapter_design_pattern.py
@@ -1,79 +1,45 @@
-from abc import ABC, abstractmethod
+class MomoPaymentAdapter:
+    def __init__(self, momo_payment):
+        self.momo_payment = momo_payment
+
+    def pay_by_visa(self, visa_payment):
+        payment_converted = MomoPaymentAdapter.convert_to_visa_payment(self.momo_payment)
+        visa_payment.pay(payment_converted)
+
+    @staticmethod
+    def convert_to_visa_payment(momo_payment):
+        conversion_rate = 24000
+        visa_amount = momo_payment.amount / conversion_rate
+        visa_payment = {
+            'card_number': momo_payment.card_number,
+            'expired_date': momo_payment.expired_date,
+            'cvv': momo_payment.cvv,
+            'amount': visa_amount,
+        }
+
+        return visa_payment
 
 
-class AudioFormat(ABC):
-    """
-    Target: AudioFormat ABC class (Existed)
-    """
-    @abstractmethod
-    def play(self):
-        pass
+class VisaPayment:
+    @staticmethod
+    def pay(payment):
+        print(f'Paying {payment["amount"]} USD with Visa card {payment["card_number"]}')
 
 
-class MP3(AudioFormat):
-    """
-    Concreted MP3 class from Target (Existed)
-    """
-    def play(self):
-        return "I am playing mp3 format"
+class MomoPayment:
+    def __init__(self, card_number, expired_date, cvv, amount):
+        self.card_number = card_number
+        self.expired_date = expired_date
+        self.cvv = cvv
+        self.amount = amount
 
 
-class ACC(AudioFormat):
-    """
-    Concreted ACC class from Target (Existed)
-    """
-    def play(self):
-        return "I am playing acc format"
+def main():
+    momo_payment = MomoPayment('123456', '12/25', 555, 24000)
+    momo_adapter = MomoPaymentAdapter(momo_payment)
+    visa_payment = VisaPayment()
+    momo_adapter.pay_by_visa(visa_payment)
 
 
-class AdvancedAudioFormat(ABC):
-    """
-    Adaptee: AdvancedAudioFormat ABC class (Existed).
-    """
-    @abstractmethod
-    def play_sound(self):
-        pass
-
-
-class WAV(AdvancedAudioFormat):
-    """
-    Concreted WAV class from Adaptee (Existed)
-    """
-    def play_sound(self):
-        return "I am playing wav format"
-
-
-class ALAC(AdvancedAudioFormat):
-    """
-    Concreted ALAC class from Adaptee (Existed)
-    """
-    def play_sound(self):
-        return "I am playing alac format"
-
-
-class AudioFormatAdapter(AudioFormat):
-    """
-    Adapter class which inherits from AudioFormat class (Target).
-    This class is used to defined which adaptee (AdvancedAudioFormat) will be played by AudioFormat.
-    """
-    def __init__(self, adaptee_):
-        self.__adaptee = adaptee_
-
-    def play(self):
-        return self.__adaptee.play_sound()
-
-
-if __name__ == "__main__":
-    mp3 = MP3()  # Create MP3 audio format
-    print(mp3.play())
-
-    acc = ACC()  # Create ACC audio format
-    print(acc.play())
-
-    wav_adaptee = WAV()  # Create WAV as advanced audio format interface
-    wav = AudioFormatAdapter(wav_adaptee)  # Transform WAV to audio format interface
-    print(wav.play())
-
-    alac_adaptee = ALAC()  # Create ALAC as advanced audio format interface
-    alac = AudioFormatAdapter(alac_adaptee)  # Transform ALAC to audio format interface
-    print(alac.play())
+if __name__ == '__main__':
+    main()
